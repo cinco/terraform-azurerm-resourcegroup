48a33cfdccb1ec50bc8cde97d4d37f35c5fd0df6
cinco <cincoecinco@gmail.com>
2020-10-15 05:56:11 -0300

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "res_group_id" {
   value = "${azurerm_resource_group.res_group.id}"
 }
+
+output "name" {
+  value = var.name
+}
+output "location" {
+  value = var.location
+}
+output "vnet_name" {
+  value = var.vnet_name
+}
+output "subnet_name" {
+  value = var.subnet_name
+}
